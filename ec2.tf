42e770016a4619b4a3fbe09146ee6ba057b9222d
Lawrence Borst <laurens.s.borst@gmail.com>
2026-09-12 02:01:45 +0200

    feat(network): add security group

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "duly-instance" {
-  ami           = "ami-051c6296b8d2535f1"
-  instance_type = "t3.small"
+  ami                    = "ami-051c6296b8d2535f1"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [aws_security_group.duly-sg.id]
 
   tags = {
     Name        = "duly-instance"
